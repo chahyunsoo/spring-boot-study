7407d737566e2f7df4862e7782b628fcf6058318
chahyunsoo <chahs1012@naver.com>
2023-06-21 18:22:19 +0900

    chore : id->Long

--- src/main/java/org/example/springbootdeveloper/MemberRepository.java
+++ src/main/java/org/example/springbootdeveloper/MemberRepository.java
@@ -1,10 +1,9 @@
 package org.example.springbootdeveloper;
 
-import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Id> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
 }
