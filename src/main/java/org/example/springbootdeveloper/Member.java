ed4868efef831d1f83eb67163d5ca965d15a1156
chahyunsoo <chahs1012@naver.com>
2023-06-25 20:01:58 +0900

    feat : CRUD Member

--- src/main/java/org/example/springbootdeveloper/Member.java
+++ src/main/java/org/example/springbootdeveloper/Member.java
@@ -6,18 +6,18 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  //기본 생성자
 @AllArgsConstructor
 @Getter
-@Entity
+@Entity //엔티티로 지정
 
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)   //primary key 자동으로 1씩 증가
     @Column(name = "id",updatable = false)
     private Long id; //DB 테이블, 'id' column
 
-    @Column(name = "name",nullable = false)
+    @Column(name = "name",nullable = false)  //'name' 이라는 not null 컬럼과 mapping
     private String name; //DB 테이블, 'name' column
 
 }
