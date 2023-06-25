3b380b980d3d89fec0850b96233b994e7df91b3a
chahyunsoo <chahs1012@naver.com>
2023-06-25 14:19:59 +0900

    test : TestControllerTest 추가

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class JUnitTest {
-    @DisplayName("단순 테스트 확인용")
+    @DisplayName("성공 테스트 확인용")
     @Test
     public void junitTest(){
         int a=1;
@@ -11,5 +11,12 @@ public class JUnitTest {
         int sum = 3;
         Assertions.assertEquals(a + b, sum);
     }
-
+    @DisplayName("실패 테스트 확인용")
+    @Test
+    public void junitTestX(){
+        int a=1;
+        int b=3;
+        int sum = 3;
+        Assertions.assertEquals(a + b, sum);
+    }
 }
